4d6d8cb46b9e380fe3dbc9c76da65b707f55d84a
Tiago Tresoldi <tiago.tresoldi@lingfil.uu.se>
2022-11-21 00:16:38 +0100

    Prepare for run with distance correction

--- pipeline/run_bayesian.py
+++ pipeline/run_bayesian.py
@@ -7,7 +7,9 @@ Run Bayesian analyses and collect results.
 # Import Python standard libraries
 from pathlib import Path
 from collections import defaultdict
+import itertools
 import glob
+import csv
 import logging
 import re
 import subprocess
@@ -23,7 +25,7 @@ BASE_PATH = Path(__file__).parent
 # Define path to executables
 BEAST2_PATH = Path("/home/tiagot/software/BEAST.v2.6.7.Linux/beast/bin")
 
-PROBLEMATIC_FAMILIES = ["bookkeeping", "easterntransfly", "mairasic", "walioic"]
+PROBLEMATIC_FAMILIES = ["bookkeeping"]
 
 
 def run_inference(BAYES_PATH, languoids):
@@ -95,7 +97,7 @@ def extract_tree(nexus):
     return tree
 
 
-def collect_global_tree(BAYES_PATH, OUTPUT_PATH):
+def collect_global_tree(family_distances, BAYES_PATH, OUTPUT_PATH):
     """
     Collect all trees into a single, global tree.
     """
@@ -106,7 +108,7 @@ def collect_global_tree(BAYES_PATH, OUTPUT_PATH):
     for tree_file in glob.glob(str(BAYES_PATH / "*.mcc.nex")):
         # Extract tree, write it to disk, and append to collection
         tree = extract_tree(tree_file)
-        tree_name = Path(tree_file).stem.split(".")[0]
+        tree_name = common.slug(Path(tree_file).stem.split(".")[0], level="full")
         if tree_name in PROBLEMATIC_FAMILIES:
             continue
 
@@ -119,6 +121,14 @@ def collect_global_tree(BAYES_PATH, OUTPUT_PATH):
         if max_dist[tree_name] < dist:
             max_dist[tree_name] = dist
 
+    # Rescale the trees in `trees` so that their biggest distance
+    # matches the one in `family_distances` (in order to later have
+    # all at the same distances from the dummy root)
+    for family, tree in trees.items():
+        bayes_dist = max_dist[family]
+        nj_dist = family_distances[family]
+        print(family, bayes_dist, nj_dist)
+
     # Build global tree
     # TODO: missing isolates
     global_tree = Tree()
@@ -126,7 +136,85 @@ def collect_global_tree(BAYES_PATH, OUTPUT_PATH):
     for family, tree in trees.items():
         global_tree.add_child(tree, dist=global_max_dist - max_dist[family])
 
-    return global_tree
+    return global_tree, trees
+
+
+def get_distances(family_trees):
+    logging.info("Collecting distances from Bayesian trees")
+
+    distances = {}
+    for family, tree in family_trees.items():
+
+        # Collect pairwise distances between leaves and deepest leaf instance
+        leaves = tree.get_leaves()
+        for lang1, lang2 in itertools.combinations_with_replacement(leaves, 2):
+            distances[lang1.name, lang2.name] = lang1.get_distance(lang2)
+
+        deepest = 0.0
+        for leaf in leaves:
+            dist = tree.get_distance(leaf)
+            if dist > deepest:
+                deepest = dist
+
+        print(family, deepest)
+
+    # for k, v in distances.items():
+    #    print(k, v)
+
+
+def dst2dict(filename):
+    """
+    Return the contents of a distance matrix files as a dictionary.
+    """
+
+    header = True
+    taxa = []
+    matrix = []
+    with open(filename, encoding="utf-8") as handler:
+        for line in handler.readlines():
+            line = re.sub(r"\s+", " ", line.strip())
+            if header:
+                header = False
+            else:
+                tokens = line.split()
+                taxa.append(tokens[0])
+                matrix.append([float(v) for v in tokens[1:]])
+
+    dst = {}
+    for taxon1, row in zip(taxa, matrix):
+        for taxon2, value in zip(taxa, row):
+            dst[taxon1, taxon2] = value
+
+    return dst
+
+
+def collect_distances(BAYES_PATH, PHYLO_PATH):
+    # Read the glottocode/doculect mapping
+    with open(BAYES_PATH / "glottomap.tsv", encoding="utf-8") as handler:
+        glottomap = list(csv.DictReader(handler, delimiter="\t"))
+        doculects = sorted([entry["Doculect"] for entry in glottomap])
+
+    # Iterate over all distance files -- while we could collect this
+    # information while computing it, for code separation it is better
+    # to just consider it external
+    file_pattern = str(PHYLO_PATH / "*.dst")
+    dst = {}
+    for dst_file in glob.glob(file_pattern):
+        family_name = Path(dst_file).stem
+        logging.info(f"Collecting precomputed distance for `{family_name}`")
+        family_dst = dst2dict(dst_file)
+        family_dst = {
+            (lang1, lang2): value
+            for (lang1, lang2), value in family_dst.items()
+            if lang1 in doculects and lang2 in doculects
+        }
+
+        # Get the family maximum distance if enough values were collected
+        values = list(family_dst.values())
+        if len(values) >= 2:
+            dst[family_name] = max(values)
+
+    return dst
 
 
 def main():
@@ -146,13 +234,23 @@ def main():
     # directory if possible
     releases = sorted(glob.glob(str(BASE_PATH.parent / "releases" / "*")))
     BAYES_PATH = Path(releases[-1]) / "bayesian"
+    PHYLO_PATH = BAYES_PATH.parent / "phylo"
     TREES_PATH = BAYES_PATH.parent / "trees"
     TREES_PATH.mkdir(exist_ok=True)
 
-    # run_inference(BAYES_PATH, languoids)
-    global_tree = collect_global_tree(BAYES_PATH, TREES_PATH)
+    # Collect the pre-computed distances for all the doculects
+    # that are part of the Bayesian tree
+    distances = collect_distances(BAYES_PATH, PHYLO_PATH)
+
+    # Run the inference for all trees
+    run_inference(BAYES_PATH, languoids)
+
+    # Build the global tree
+    global_tree, family_trees = collect_global_tree(distances, BAYES_PATH, TREES_PATH)
     with open(TREES_PATH / "global.tree", "w") as handler:
         handler.write(global_tree.write(format=1))
+
+    get_distances(family_trees)
 
 
 if __name__ == "__main__":
